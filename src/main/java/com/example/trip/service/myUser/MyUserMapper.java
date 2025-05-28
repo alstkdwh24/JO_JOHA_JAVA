d947a8262ac29956c94e9c2304eab94e11f6302c
alstkdwh24 <alstkdwh24@naver.com>
2025-05-28 23:00:23 +0900

    다시 프론트로

--- src/main/java/com/example/trip/service/myUser/MyUserMapper.java
+++ src/main/java/com/example/trip/service/myUser/MyUserMapper.java
@@ -2,7 +2,9 @@ package com.example.trip.service.myUser;
 
 import com.example.trip.commendVO.JoJoHaVO;
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.context.annotation.Lazy;
 
+@Lazy
 @Mapper
 public interface MyUserMapper{
 
